8438496b9dc2a9e317504c0bf4f57b24ae0d171e
Maja Thunberg <majaneko@gmail.com>
2021-10-01 16:27:23 +0200

    Setup express server starter project

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const apiRouter = require('./routes/api');
+
+const app = express();
+const PORT = 5000;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/api', apiRouter);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+module.exports = app;
