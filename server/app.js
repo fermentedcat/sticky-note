01fd158ed510904b18ca27175b64e1c87d8e9cf6
Maja Thunberg <majaneko@gmail.com>
2021-10-06 15:16:27 +0200

    Install cors

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const indexRouter = require('./routes/index')
 const apiRouter = require('./routes/api')
@@ -26,6 +27,7 @@ db.once('open', () => {
 })
 
 app.use(logger('dev'))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
