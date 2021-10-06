50deb10c6321f117e767579d1139fdbab0eda0f5
Maja Thunberg <majaneko@gmail.com>
2021-10-06 14:41:04 +0200

    Setup basic routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
+import { Redirect, Route, Switch } from 'react-router-dom';
 
+import HomePage from './pages/HomePage';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
+  const isAuthenticated = false;
+
   return (
-    <div>
-    </div>
+    <Switch>
+      <Route path="/" exact>
+        {isAuthenticated ? <HomePage /> : <LandingPage />}
+      </Route>
+      <Route path="/login">
+        {isAuthenticated ? <Redirect to="/" /> : <LoginPage />}
+      </Route>
+      <Route path="/register">
+        {isAuthenticated ? <Redirect to="/" /> : <RegisterPage />}
+      </Route>
+    </Switch>
   );
 }
 
---
+++ client/src/pages/RegisterPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function RegisterPage() {
+  return (
+    <div>
+      Register
+      <Link to='/login'>Login</Link>
+    </div>
+  )
+}
