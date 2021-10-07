c5e7bfe7bc908c19a115c6f9846da1381d6a7671
Maja Thunberg <majaneko@gmail.com>
2021-10-07 21:37:13 +0200

    Add addNewUser api call fn

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -7,3 +7,17 @@ export const getUserData = async (userId) => {
   }
   return await response.json()
 }
+
+export const addNewUser = async (userData) => {
+  const response = await fetch(`${API_URL}/user`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(userData)
+  })
+  if (!response.ok) {
+    throw new Error('Error fetching user data.')
+  }
+  return await response.json()
+}
