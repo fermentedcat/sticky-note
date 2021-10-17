1869018ca2f72c723dd47091a09b22663fedbc28
Maja Thunberg <majaneko@gmail.com>
2021-10-17 17:53:52 +0200

    Add lastEdit, lastEditBy, stack id, markdown

--- server/models/Todo.js
+++ server/models/Todo.js
@@ -1,15 +1,37 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const marked = require('marked')
+const slugify = require('slugify')
+const createDomPurify = require('dompurify')
+const { JSDOM } = require('jsdom')
+const dompurify = createDomPurify(new JSDOM().window)
+
+const validate = require('../utils/validate')
 
 
 const TodoSchema = new Schema({
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
+      validate: [
+        validate.string, 
+        'Please enter a valid name.'
+      ],
     },
-    body: {
-        type: String,
-        required: true
+    stack: {
+      type: Schema.Types.ObjectId, 
+      ref: 'Stack'
+    },
+    description: {
+      type: String
+    },
+    markdown: {
+      type: String,
+      required: true
+    },
+    sanitizedHtml: {
+      type: String,
+      required: true
     },
     creator: {
       type: Schema.Types.ObjectId, 
@@ -19,8 +41,36 @@ const TodoSchema = new Schema({
       type: Boolean,
       required: false
     },
+    lastEditBy: {
+      type: Schema.Types.ObjectId, 
+      ref: 'User',
+    },
+    lastEdit: {
+      type: String,
+    }
 }, {
   timestamps: true,
+})
+
+// PRE VALIDATE:
+
+// set default lastEditBy to creator
+TodoSchema.pre('validate', function (next, done) {
+  // sanitize markdown into sanitizedHtml
+  if (this.isModified('lastEditBy')) {
+    return next()
+  }
+  this.lastEditBy = this.creator
+  next()
+})
+
+TodoSchema.pre('validate', function (next, done) {
+  // sanitize markdown into sanitizedHtml
+  if (!this.isModified('markdown')) {
+    return next()
+  }
+  this.sanitizedHtml = dompurify(marked(this.markdown))
+  next()
 })
 
 module.exports = mongoose.model('Todo', TodoSchema)
