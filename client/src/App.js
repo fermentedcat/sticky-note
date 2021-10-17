36dabea0341afdb49e7aad01b779113a08a8fc75
Maja Thunberg <majaneko@gmail.com>
2021-10-17 08:13:17 +0200

    Authenticate user token on refresh

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import { Redirect, Route, Switch } from 'react-router-dom';
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { authenticate } from './store/auth-slice'
 
 import HomePage from './pages/HomePage';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import { useEffect } from 'react';
 
 function App() {
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(authenticate())
+  }, [dispatch])
 
   return (
     <Switch>
--- client/src/store/auth-slice.js
+++ client/src/store/auth-slice.js
@@ -1,8 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit"
+import jwtDecode from 'jwt-decode'
+import axios from 'axios';
+const API_URL = process.env.REACT_APP_API_URL
+
 
 const initialAuthSlice = {
   isAuthenticated: false,
-  userId: ''
+  token: null,
+  username: null
 }
 
 const authSlice = createSlice({
@@ -10,10 +15,16 @@ const authSlice = createSlice({
   initialState: initialAuthSlice,
   reducers: {
     login(state, action) {
+      const decoded = jwtDecode(action.payload)
+      const username = decoded.username || 'buddy'
       state.isAuthenticated = true
+      state.token = action.payload
+      state.username = username
     },
-    logout(state, action) {
-      state.isAuthenticated = false
+    logout(state) {
+      state.isAuthenticated = initialAuthSlice.isAuthenticated
+      state.token = initialAuthSlice.token
+      state.username = initialAuthSlice.username
     },
   },
   extraReducers: {}
@@ -21,3 +32,26 @@ const authSlice = createSlice({
 
 export default authSlice.reducer
 export const authActions = authSlice.actions
+
+export const authenticate = () => {
+  return async dispatch => {
+    const token = window.localStorage.getItem('TODO_TOKEN')
+    try {
+      await axios({ 
+        url: `${API_URL}user/auth`, 
+        headers: { 
+          'x-auth-token': token
+        } 
+      });
+      dispatch(authActions.login(token))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+export const isValidToken = () => {
+  const { exp } = jwtDecode(authSlice.token)
+  const expirationTime = (exp * 1000) - 60000
+  return Date.now() >= expirationTime
+}
