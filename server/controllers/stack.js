282c64833a95899761479e5cb5bab1386abcb0dd
Maja Thunberg <majaneko@gmail.com>
2021-10-18 18:08:09 +0200

    Add getBySlug, get by access in getAll

--- server/controllers/stack.js
+++ server/controllers/stack.js
@@ -3,87 +3,92 @@ const Access = require('../models/Access')
 const Todo = require('../models/Todo')
 const User = require('../models/User')
 
-exports.getAll = (req, res, next) => {
-  //TODO: check req.user.role === admin || user id === stack owner || access user id ≈≈≈ req user id
-  Access.find()
-  .then(accesses => {
-    if (!accesses) res.sendStatus(404)
-    else res.status(200).json(accesses)
-  })
-  .catch(error => {
+exports.getAll = async (req, res, next) => {
+  const { userId, role } = req.user
+
+  try {
+    // get all if admin
+    if (role === 'admin') {
+      const stacks = await Stack.find()
+      if (!stacks) res.sendStatus(404).send('Found nothing.')
+      else res.status(200).json(stacks)
+    }
+
+    // make array of and find stacks user has access to (or is owner of)
+    const accesses = await Access.find({ 'user._id': userId })
+    const accessArr = accesses.map((access) => access.stack)
+    const stacks = await Stack.find({
+      $or: [{ _id: { $in: accessArr } }, { owner: userId }],
+    })
+
+    if (stacks.length === 0) res.sendStatus(404).send('Found nothing.')
+    res.status(200).json(stacks)
+  } catch (error) {
     res.status(500).json(error)
-  })
+  }
 }
 
-exports.getAllByUserId = (req, res, next) => {
-  //TODO: check req.user.role === admin || user id === stack owner || access user id ≈≈≈ req user id
-  Access.find()
-  .then(accesses => {
-    if (!accesses) res.sendStatus(404)
-    else res.status(200).json(accesses)
-  })
-  .catch(error => {
-    res.status(500).json(error)
-  })
-}
+exports.getBySlug = async (req, res, next) => {
+  const slug = req.params.slug
+  const { userId, role } = req.user
 
-exports.getById = (req, res, next) => {
-  //TODO: check req.user.role === admin || user id === stack owner || access user id ≈≈≈ req user id
-  const id = req.params.id
-  Access.findById(id)
-  .then(access => {
-    if (!access) res.sendStatus(404)
-    else res.status(200).json(access)
-  })
-  .catch(error => {
+  try {
+    const stack = await Stack.findOne({ slug: slug })
+    if (!stack) res.sendStatus(404)
+
+    // check if user has access by invite
+    const accesses = await Access.find({ stack: stack._id })
+    const hasAccess = accesses.some((doc) => doc.user._id == userId)
+
+    if (stack.owner != userId && role !== 'admin' && !hasAccess) {
+      res.send(401).send('Unauthorized')
+    }
+
+    // find stack's todo lists
+    const todos = await Todo.find({ stack: stack._id })
+
+    res.status(200).json({ stack, todos })
+  } catch (error) {
     res.status(500).json(error)
-  })
+  }
 }
 
 exports.addNew = (req, res, next) => {
-  // const data = req.body
-  const data = {
-    title: 'Favorites',
-    owner: '616c428118648f7f8df5248c',
-    description: 'some description'
-  }
+  const data = req.body
+
   new Stack(data)
-  .save()
-  .then(stack => {
-    res.status(201).json(stack)
-  })
-  .catch(error => {
-    res.status(500).json(error)
-  })
+    .save()
+    .then((stack) => {
+      res.status(201).json(stack)
+    })
+    .catch((error) => {
+      res.status(500).json(error)
+    })
 }
 
 exports.update = (req, res, next) => {
-  //TODO: check req.user.role === admin ||  user id === stack owner || access user id ≈≈≈ req user id
+  //TODO: check req.user.role === admin ||  user id === stack owner || Stack user id ≈≈≈ req user id
 
   const id = req.params.id
   const data = req.body
-  Access.findByIdAndUpdate(
-    id, 
-    data, 
-    { new: true }
-  )
-  .then(access => {
-    res.status(201).json(access)
-  })
-  .catch(error => {
-    res.status(500).json(error)
-  })
+  Stack.findByIdAndUpdate(id, data, { new: true })
+    .then((stack) => {
+      res.status(201).json(stack)
+    })
+    .catch((error) => {
+      res.status(500).json(error)
+    })
 }
 
 exports.delete = (req, res, next) => {
   //TODO: check req.user.role === admin || req user id === stack owner
   const id = req.params.id
-  Access.findByIdAndDelete(id)
-  .then((access) => {
-    if (!access) res.sendStatus(404)
-    else res.sendStatus(204)
-  })
-  .catch(error => {
-    res.status(500).json(error)
-  })
+  Stack.findByIdAndDelete(id)
+    .then((stack) => {
+      if (!stack) res.sendStatus(404)
+      else res.sendStatus(204)
+    })
+    .catch((error) => {
+      res.status(500).json(error)
+    })
 }
