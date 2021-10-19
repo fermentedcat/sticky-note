afdfc9f4d74b451c72487b69e7134bb952522e6b
Maja Thunberg <majaneko@gmail.com>
2021-10-19 11:36:28 +0200

    Add pinned & all liks

--- client/src/components/layout/SideBar.js
+++ client/src/components/layout/SideBar.js
@@ -14,57 +14,79 @@ const Item = styled(Paper)(({ theme }) => ({
 }))
 
 export default function SideBar() {
-  const { 
+  const {
     data: stacks,
     error,
     token,
     callGet,
     callPost,
     callDelete,
-    setData } = useApi('stack')
+    setData,
+  } = useApi('stack')
 
   return (
-    <Grid 
-      item 
+    <Grid
+      item
       xs={2}
-      sx={{ 
+      sx={{
         height: '100%',
         minWidth: '240px',
         boxShadow: '-2px 0px 19px 0px rgba(255,191,209,0.34)',
         paddingTop: 10,
       }}
-      >
-        <Box sx={{ flexGrow: 1 }}>
+    >
+      <Box sx={{ flexGrow: 1 }}>
+        {error && (
           <Typography
-            variant="h6" 
-            component="h2" 
+            variant="h6"
+            component="h2"
             align="center"
-            sx={{ fontSize: '14px',  marginTop: 3, color: 'rgb(25, 118, 210)'}}
+            sx={{ fontSize: '14px', marginTop: 3, color: 'rgb(25, 118, 210)' }}
           >
-            YOUR STACKS
+            {error}
           </Typography>
-          <Grid
-            container
-            direction="column"
-            spacing={{ xs: 1, md: 2 }}
-            padding={2}
-          >
-            {stacks && stacks.map(stack => {
+        )}
+
+        <Typography
+          variant="h6"
+          component="h2"
+          align="center"
+          sx={{ fontSize: '14px', marginTop: 3, color: 'rgb(25, 118, 210)' }}
+        >
+          YOUR STACKS
+        </Typography>
+
+        <Grid
+          container
+          direction="column"
+          spacing={{ xs: 1, md: 2 }}
+          padding={2}
+        >
+          <Grid component={Link} to="/todo/pinned" item xs={2} sm={4} md={4}>
+            <Item>Pinned</Item>
+          </Grid>
+
+          {stacks &&
+            stacks.map((stack) => {
               return (
-                <Grid 
-                  key={stack._id} 
+                <Grid
+                  key={stack._id}
                   component={Link}
                   to={`/stack/${stack.slug}`}
-                  item 
-                  xs={2} 
-                  sm={4} 
+                  item
+                  xs={2}
+                  sm={4}
                   md={4}
-                  >
+                >
                   <Item>{stack.title}</Item>
                 </Grid>
               )
             })}
+
+          <Grid component={Link} to="/todo/all" item xs={2} sm={4} md={4}>
+            <Item>All todo's</Item>
           </Grid>
+        </Grid>
       </Box>
     </Grid>
   )
