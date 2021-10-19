be6f7a9bb1c9e4b8f0d91304db3a77118b270fa2
Maja Thunberg <majaneko@gmail.com>
2021-10-19 14:32:53 +0200

    Add todo formFields

--- client/src/utils/formFields.js
+++ client/src/utils/formFields.js
@@ -39,9 +39,32 @@ const fullName = {
     initialValue: '',
     validate: validate.string,
     required: true,
+  },
+  title = {
+    type: 'text',
+    name: 'title',
+    label: 'Title',
+    initialValue: 'My New Todo',
+    validate: validate.string,
+    required: true,
+  },
+  description = {
+    type: 'text',
+    name: 'description',
+    label: 'Description',
+    initialValue: 'Todo list description',
+    validate: validate.string,
+  },
+  markdown = {
+    type: 'text',
+    name: 'markdown',
+    label: '',
+    initialValue: 'My new todo list',
+    validate: validate.string,
   }
 
 const register = { fullName, username, email, password }
 export default register
 
 export const login = { loginName, password }
+export const todo = { title, description, markdown }
