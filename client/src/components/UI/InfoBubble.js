b70ec33e139cba08530f459a95e38cd5d6300cd0
Maja Thunberg <majaneko@gmail.com>
2021-10-19 11:35:00 +0200

    Add icon info bubble

---
+++ client/src/components/UI/InfoBubble.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import classes from './InfoBubble.module.css'
+
+export default function InfoBubble({ text, bottom }) {
+  let bubbleClasses = classes.bubble;
+
+  if (bottom) {
+    bubbleClasses += ` ${classes.bottom}`
+  }
+
+  return (
+    <div className={bubbleClasses}>
+      <div className={classes.textBox}>
+        <p>{text}</p>
+      </div>
+      <div className={classes.triangle}></div>
+    </div>
+  )
+}
