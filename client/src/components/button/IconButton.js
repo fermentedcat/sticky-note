d5adaae55b2660d3d7dea9ccdd89c81ead338b37
Maja Thunberg <majaneko@gmail.com>
2021-10-19 11:34:43 +0200

    Add icons

---
+++ client/src/components/button/IconButton.js
@@ -0,0 +1,135 @@
+import React, { useState } from 'react'
+
+import classes from './IconButton.module.css'
+
+import { IoEllipsisVerticalSharp, IoExitOutline } from 'react-icons/io5'
+import { AiOutlineInfoCircle } from 'react-icons/ai'
+import { FiSettings, FiEdit } from 'react-icons/fi'
+import { CgFormatSlash } from 'react-icons/cg'
+import { VscSmiley } from 'react-icons/vsc'
+import {
+  RiLock2Fill,
+  RiDeleteBin6Line,
+  RiPushpinLine,
+  RiPushpinFill,
+} from 'react-icons/ri'
+import { ImQuotesLeft } from 'react-icons/im'
+import { ImCancelCircle } from 'react-icons/im'
+import { HiOutlineBookOpen } from 'react-icons/hi'
+
+import InfoBubble from '../UI/InfoBubble'
+import { Box } from '@mui/material'
+
+export default function IconButton({
+  type = 'like',
+  size = '1.4',
+  m = '.05',
+  p = '.3',
+  circle = true,
+  active,
+  description,
+  last,
+  bottomDescr,
+  onClick = () => {},
+  onMouseEnter = () => {},
+  onMouseLeave = () => {},
+}) {
+  const [showDescription, setShowDescription] = useState(false)
+  
+  const wrapperClasses = `${classes.icon} ${active ? classes.active : ''}`
+  
+  const styles = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: `${p}em`,
+    height: 'fit-content',
+    borderRadius: circle ? '50%' : '3px',
+    fontSize: `${size}em`,
+    margin: `${m}em`,
+  }
+
+  const toggleDescription = () => {
+    setShowDescription(!showDescription)
+  }
+  
+  const buttonProps = {
+    onClick,
+    onMouseEnter,
+    onMouseLeave,
+  }
+
+  if (description) {
+    buttonProps.onMouseEnter = toggleDescription
+    buttonProps.onMouseLeave = toggleDescription
+  }
+  
+  let icon
+  
+  switch (type) {
+    case 'actions':
+      icon = <IoEllipsisVerticalSharp />
+      break
+
+    case 'settings':
+      icon = <FiSettings />
+      break
+
+    case 'info':
+      icon = <AiOutlineInfoCircle />
+      break
+
+    case 'fwdslash':
+      icon = <CgFormatSlash />
+      break
+
+    case 'lock':
+      icon = <RiLock2Fill />
+      break
+
+    case 'quote':
+      icon = <ImQuotesLeft />
+      break
+
+    case 'read':
+      icon = <HiOutlineBookOpen />
+      break
+
+    case 'edit':
+      icon = <FiEdit />
+      break
+
+    case 'delete':
+      icon = <RiDeleteBin6Line />
+      break
+
+    case 'pin':
+      icon = <RiPushpinLine />
+      break
+
+    case 'unpin':
+      icon = <RiPushpinFill className={classes.pinned}/>
+      break
+
+    case 'cancel':
+      icon = <ImCancelCircle />
+      break
+
+    case 'exit':
+      icon = <IoExitOutline />
+      break
+
+    default:
+      icon = <VscSmiley />
+      break
+  }
+
+  return (
+    <Box {...buttonProps} className={wrapperClasses} sx={styles}>
+      {showDescription && (
+        <InfoBubble text={description} last={last} bottom={bottomDescr} />
+      )}
+      {icon}
+    </Box>
+  )
+}
