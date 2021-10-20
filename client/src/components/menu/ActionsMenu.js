c400f5c89ea891f6760cc9c297e50ef46f44e1c3
Maja Thunberg <majaneko@gmail.com>
2021-10-20 18:34:44 +0200

    Remove unused functionality

--- client/src/components/menu/ActionsMenu.js
+++ client/src/components/menu/ActionsMenu.js
@@ -1,49 +1,25 @@
-import { List, ListItem } from '@mui/material';
+import { List, ListItem } from '@mui/material'
 import React, { Children } from 'react'
 import classes from './ActionsMenu.module.css'
 
-export default function ActionsMenu({
-  children, 
-  onMouseLeave,
-  invisible,
-  bottomDescr = false
-}) {
-  const childrenArray = Children.toArray(children);
+export default function ActionsMenu({ children, onMouseLeave }) {
+  const childrenArray = Children.toArray(children)
 
-  let listClasses = `${classes.menu}`
-
-  if (invisible) {
-    listClasses += ` ${classes.invisible}`;
-  }
-
-  // add last = true if last child element (for description display)
-  const childrenWithProps = Children.map(childrenArray, (child, index) => {
-    if (React.isValidElement(child)) {
-      let last = false
-      if (childrenArray.length === index + 1) {
-        last = true
-      }
-      return React.cloneElement(child, {last, bottomDescr})
-    }
-    return child;
-  })
-
-  const list = childrenWithProps.map((child, index) => {
+  const list = childrenArray.map((child, index) => {
     return (
-      <ListItem key={index} className={classes.menuItem} /* action */>
+      <ListItem key={index} className={classes.menuItem}>
         {child}
       </ListItem>
     )
   })
 
   return (
-    <List 
-      className={listClasses}
-      onMouseLeave={onMouseLeave} 
-      sx={{ padding: 0}}
-      >
+    <List
+      className={classes.menu}
+      onMouseLeave={onMouseLeave}
+      sx={{ padding: 0 }}
+    >
       {list}
     </List>
-
   )
 }
