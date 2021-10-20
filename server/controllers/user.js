11e8abbbd3d4338dd9083a6f54924fc30245c0fa
Maja Thunberg <majaneko@gmail.com>
2021-10-20 23:39:10 +0200

    Refactor user api

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,47 +4,75 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 
 /* 
+GET     /api/user/auth  - authenticate user
+GET     /api/user/me   - get logged in user
+GET     /api/user/search/:string   - get all by part of username
 GET     /api/user/      - get all users (friends) for logged in user
 GET     /api/user/:id   - get one user
+
+POST    /api/user/login  - login user
+POST    /api/user/addPin  - add pinned
+POST    /api/user/removePin  - remove pinned todo
 POST    /api/user/      - add new user
 POST    /api/user/:id   - update user
+
 DELETE  /api/user/:id   - delete user
 */
-
-exports.getAll = (req, res, next) => {
-  User.find()
-    .then((users) => {
-      if (!users) res.sendStatus(404)
-      else res.status(200).json(users)
-    })
-    .catch((error) => {
-      res.status(500).json(error)
-    })
-}
-
-exports.getById = (req, res, next) => {
-  const id = req.params.id
-  User.findById(id)
-    .then((user) => {
-      if (!user) res.sendStatus(404)
-      else res.status(200).json(user)
-    })
-    .catch((error) => {
-      res.status(500).json(error)
-    })
-}
 
 exports.authenticate = (req, res, next) => {
   res.status(200).json(req.user)
 }
 
+exports.getLoggedInUser = async (req, res, next) => {
+  const { username } = req.user
+  try {
+    const user = await User.findOne({ username })
+    if (!user) res.sendStatus(404)
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+exports.getAllByUsername = async (req, res, next) => {
+  const string = req.params.string
+  try {
+    const users = await User.find({
+      username: { $regex: string, $options: 'i' },
+    })
+    res.status(200).json(users)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+exports.getAll = async (req, res, next) => {
+  try {
+    const users = await User.find()
+    res.status(200).json(users)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+exports.getById = async (req, res, next) => {
+  const id = req.params.id
+  try {
+    const user = await User.findById(id)
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
 exports.login = async (req, res, next) => {
   const { email, password } = req.body
-  const user = await User.findOne({
-    $or: [{ email: email }, { username: email }],
-  }).select('password username role')
 
   try {
+    const user = await User.findOne({
+      $or: [{ email: email }, { username: email }],
+    }).select('password username role')
+
     const isMatch = await bcrypt.compare(password, user.password)
     if (!isMatch) {
       throw new Error('Failed to login.')
@@ -61,67 +89,70 @@ exports.login = async (req, res, next) => {
     )
     res.status(200).send(token)
   } catch (error) {
-    res.status(401).json({ error: error.message })
+    res.status(401).json(error)
   }
 }
 
-exports.addNew = (req, res, next) => {
-  const userData = req.body
+exports.addTodoPin = (req, res, next) => {
+  const { username } = req.user
+  const todoId = req.body.todoId
 
-  new User(userData).save((error, user) => {
-    if (error) {
-      const err = format.error(error)
-      res.status(400).json(err)
-    }
-    // Create token
-    const token = jwt.sign({ username: user.username }, process.env.TOKEN_KEY, {
-      expiresIn: process.env.TOKEN_EXPIRATION,
-    })
-    res.status(200).json({ token: token })
-  })
-}
-
-exports.update = (req, res, next) => {
-  const id = req.params.id
-  const data = req.body
-  User.findByIdAndUpdate(id, data, { new: true })
-    .then((user) => {
-      res.status(201).json(user)
+  User.findOneAndUpdate({ username }, { $push: { pinnedTodos: todoId } })
+    .then(() => {
+      res.status(200).send('Saved')
     })
     .catch((error) => {
       res.status(500).json(error)
     })
 }
 
-exports.addTodoPin = async (req, res, next) => {
-  const { userId } =req.user
+exports.removeTodoPin = (req, res, next) => {
+  const { username } = req.user
   const todoId = req.body.todoId
 
-  User.findByIdAndUpdate(userId, { $push: { pinnedTodos: todoId } })
-  .then(() => {
-    res.status(200).send('Saved')
-  })
-  .catch((error) => {
-    res.status(500).json(error)
-  })
+  User.findOneAndUpdate({ username }, { $pull: { pinnedTodos: todoId } })
+    .then(() => {
+      res.status(200).send('Saved')
+    })
+    .catch((error) => {
+      res.status(500).json(error)
+    })
 }
 
-exports.removeTodoPin = async (req, res, next) => {
-  const { userId } =req.user
-  const todoId = req.body.todoId
+exports.addNew = async (req, res, next) => {
+  const userData = req.body
 
-  User.findByIdAndUpdate(userId, { $pull: { pinnedTodos: todoId } })
-  .then(() => {
-    res.status(200).send('Saved')
-  })
-  .catch((error) => {
-    res.status(500).json(error)
-  })
+  try {
+    const user = await new User(userData).save()
+    // Create token
+    const token = jwt.sign({
+      username: user.username,
+      userId: user._id,
+      role: user.role,
+    }, process.env.TOKEN_KEY, {
+      expiresIn: process.env.TOKEN_EXPIRATION,
+    })
+    res.status(200).send(token)
+  } catch (error) {
+    res.status(400).json(error)
+  }
+}
+
+exports.update = async (req, res, next) => {
+  const username = req.params.username
+  const data = req.body
+
+  try {
+    const user = await User.findOneAndUpdate({ username }, data, { new: true })
+    res.status(201).json(user)
+  } catch (error) {
+    res.status(400).json(error)
+  }
 }
 
 exports.delete = (req, res, next) => {
-  const id = req.params.id
-  User.findByIdAndDelete(id)
+  const username = req.params.username
+  User.findOneAndDelete({ username })
     .then((user) => {
       if (!user) res.sendStatus(404)
       else res.sendStatus(204)
--- server/routes/user.js
+++ server/routes/user.js
@@ -8,10 +8,14 @@ const user = require('../controllers/user')
 
 /* 
 GET     /api/user/auth  - authenticate user
+GET     /api/user/me   - get logged in user
+GET     /api/user/search/:string   - get all by part of username
 GET     /api/user/      - get all users (friends) for logged in user
 GET     /api/user/:id   - get one user
 
 POST    /api/user/login  - login user
+POST    /api/user/addPin  - add pinned
+POST    /api/user/removePin  - remove pinned todo
 POST    /api/user/      - add new user
 POST    /api/user/:id   - update user
 
@@ -19,15 +23,17 @@ DELETE  /api/user/:id   - delete user
 */
 
 router.get('/auth', userAuth, user.authenticate)
-router.get('/', user.getAll)
-router.get('/:id', user.getById)
+router.get('/me', userAuth, user.getLoggedInUser)
+router.get('/search/:string', user.getAllByUsername)
+router.get('/', adminAuth, user.getAll)
+router.get('/:id', adminAuth, user.getById)
 
 router.post('/login', user.login)
 router.post('/addPin', userAuth, user.addTodoPin)
 router.post('/removePin', userAuth, user.removeTodoPin)
 router.post('/', user.addNew)
-router.post('/:id', user.update)
+router.post('/:username', userAuth, user.update)
 
-router.delete('/user/:id', user.delete)
+router.delete('/user/:id', userAuth, user.delete)
 
 module.exports = router
