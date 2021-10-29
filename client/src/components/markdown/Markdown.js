7dfd82bb202a59dde88ca7d87c47f000f94fabd3
Maja Thunberg <majaneko@gmail.com>
2021-10-29 09:53:54 +0200

    Style small card markdown

--- client/src/components/markdown/Markdown.js
+++ client/src/components/markdown/Markdown.js
@@ -3,9 +3,10 @@ import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import style from './Markdown.module.css'
 
-export default function Markdown({ text }) {
+export default function Markdown({ text, small }) {
+  const styles = `${style.markdown} ${small ? style.small : ''}`
   return (
-    <ReactMarkdown remarkPlugins={[remarkGfm]} className={style.markdown}>
+    <ReactMarkdown remarkPlugins={[remarkGfm]} className={styles}>
       {text}
     </ReactMarkdown>
   )
