00528832751a12e5262737f15fa38b236cd3ee89
Maja Thunberg <majaneko@gmail.com>
2021-10-29 10:08:48 +0200

    Fix pre validate bug

--- server/models/Access.js
+++ server/models/Access.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const validate = require('../utils/validate')
 const { CustomError, InternalError } = require('../utils/error')
 
 const AccessSchema = new Schema({
@@ -9,47 +8,32 @@ const AccessSchema = new Schema({
     ref: 'Stack'
   },
   user: {
-    _id: {
-      type: Schema.Types.ObjectId,
-      ref: 'User'
-    },
-    email: {
-      type: String,
-      validate: [
-        validate.email,
-        'Invalid email'
-      ]
-    }
+    type: Schema.Types.ObjectId,
+    ref: 'User'
   }
 })
 
 // PRE VALIDATE:
 
 // check if matching access doc already exists when saving new access
-AccessSchema.pre('validate', function (next, done) {
-  this.constructor.findOne({
-    $and: [
-      { collection: this.collection },
-      {
-        $or: [
-          { 'user._id': this.user._id },
-          { 'user.email': this.user.email }
-        ]
-      }
-    ]
-  })
-    .then(access => {
-    // if match on email, prevent duplicate doc
-      if (access) {
-        const err = new CustomError('user', 'User already has access to this collection.')
-        next(err)
-      }
-      next()
+AccessSchema.pre('validate', async function (next, done) {
+  try {
+    const access = await this.constructor.findOne({
+      stack: this.stack,
+      user: this.user
     })
-    .catch(() => {
-      const err = new InternalError('internal', 'Database error.')
+    if (access) {
+      const err = new CustomError(
+        'user',
+        'User already has access to this stack.'
+      )
       next(err)
-    })
+    }
+    next()
+  } catch (error) {
+    const err = new InternalError(error)
+    next(err)
+  }
 })
 
 module.exports = mongoose.model('Access', AccessSchema)
