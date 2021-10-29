56fc3f13165ea91b63731ee0f56b642a307e76f6
Maja Thunberg <majaneko@gmail.com>
2021-10-29 09:54:55 +0200

    Modify stack form to handle edit stack

--- client/src/components/form/StackForm.js
+++ client/src/components/form/StackForm.js
@@ -1,20 +1,29 @@
 import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
-import { addStack, updateStack } from '../../store/stack-actions'
+import { addStack, deleteStack, updateStack } from '../../store/stack-actions'
 import useInput from '../../hooks/use-input'
 import { stack } from '../../utils/formFields'
 
-import SubmitButton from '../button/SumbitButton'
 import FormBox from './FormBox'
-import { TextField } from '@mui/material'
+import { Button, TextField, Box } from '@mui/material'
+import { useHistory } from 'react-router'
+import { SearchField } from './SearchField'
+import Card from '../card/Card'
 
-export default function StackForm({ stackData = {}, closeForm }) {
+export default function StackForm({ edit, closeForm }) {
   const [formIsValid, setFormIsValid] = useState(false)
+  const { stack: currentStack } = useSelector((state) => state.todo)
   const dispatch = useDispatch()
+  const history = useHistory()
 
-  const titleInitVal = stackData.title || stack.title.initialValue
-  const descrInitVal = stackData.description || stack.description.initialValue
+  let titleInitVal = stack.title.initialValue
+  let descrInitVal = stack.description.initialValue
+
+  if (edit && stack) {
+    titleInitVal = currentStack.title
+    descrInitVal = currentStack.description
+  }
 
   const titleInput = useInput(stack.title.validate, titleInitVal)
   const descrInput = useInput(stack.description.validate, descrInitVal)
@@ -49,22 +58,45 @@ export default function StackForm({ stackData = {}, closeForm }) {
       description: descrInput.value,
     }
     // edit existing or add new todo
-    if (stackData._id) {
-      dispatch(updateStack({ ...data, _id: stackData._id }))
+    if (edit) {
+      dispatch(updateStack({ ...data, _id: currentStack._id }))
       closeForm()
       return
     }
     dispatch(addStack(data))
     closeForm()
   }
+
+  const handleDeleteStack = async () => {
+    if (
+      !confirm(
+        'Are you sure you want to delete this stack? This will permantntly delete all of its todo lists.'
+      )
+    ) {
+      return
+    }
+    const response = await dispatch(deleteStack(currentStack._id))
+    if (response.meta.requestStatus === 'fulfilled') {
+      console.log(response.meta.requestStatus)
+      history.push('/todo/all')
+      closeForm()
+    }
+  }
+
   useEffect(() => {
     setFormIsValid(titleInput.isValid)
   }, [titleInput.isValid])
 
   return (
-    <FormBox onSubmit={handleSubmitStack}>
-      {inputFields}
-      <SubmitButton title="Save" />
-    </FormBox>
+    <Card title={edit ? 'Edit stack' : 'Add stack'}>
+      <FormBox onSubmit={handleSubmitStack}>
+        {inputFields}
+        <SearchField />
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <Button type="submit">Save</Button>
+          {edit && <Button onClick={handleDeleteStack}>Delete</Button>}
+        </Box>
+      </FormBox>
+    </Card>
   )
 }
