dd2b80d992e42ff21d5558a597269942a76adcf1
Maja Thunberg <majaneko@gmail.com>
2021-11-01 17:21:04 +0100

    Add info to about page

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -1,14 +1,181 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Button, Box } from '@mui/material'
+import { Button, Box, Container, Typography, IconButton } from '@mui/material'
+import LogoCard from '../components/layout/LogoCard'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import { useSelector } from 'react-redux'
 
 export default function AboutPage() {
+  const isAuthenticated = useSelector((state) => state.user.isAuthenticated)
+
   return (
-    <Box sx={{ my: 'auto' }}>
-      This is your favorite todo app.
-      <Button component={Link} to="/login">
-        Login or register an account!
-      </Button>
-    </Box>
+    <Container
+      sx={{
+        width: 'fit-content',
+        height: '60vh',
+        mx: 'auto',
+        my: 'auto',
+        display: 'flex',
+      }}
+    >
+      <Box
+        sx={{
+          my: 'auto',
+          mr: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          width: '60vh',
+        }}
+      >
+        <Typography
+          variant="p"
+          sx={{
+            p: 1,
+            mt: 2,
+          }}
+        >
+          <b>Sticky Note</b> is a todo app where your todo lists are written
+          using markdown. You can use this{' '}
+          <span>
+            <a href="https://www.markdownguide.org/cheat-sheet/">
+              Markdown Cheat Sheet
+            </a>
+          </span>{' '}
+          for instructions.
+        </Typography>
+        <Typography
+          variant="p"
+          sx={{
+            p: 1,
+            m: 0,
+            fontWeight: 'bold',
+          }}
+        >
+          Create stacks and todo lists
+        </Typography>
+        <Typography
+          variant="p"
+          sx={{
+            p: 1,
+            m: 0,
+          }}
+        >
+          To create a todo list, you need to first create a stack to put it in.
+          Hit the Add Stack button in the sidebar to create it. You can edit it
+          later.
+        </Typography>
+        <Typography
+          variant="p"
+          sx={{
+            p: 1,
+            m: 0,
+          }}
+        >
+          Once you&apos;re in the stack, click Add Todo to create your todo
+          list.
+        </Typography>
+        <Typography
+          variant="p"
+          sx={{
+            p: 1,
+            m: 0,
+          }}
+        >
+          All of your todo lists will be shown under All Todo&apos;s. You can
+          also pin a todo list to collect the ones you use often. Click the pin
+          icon on the todo item and it will show up under Pinned.
+        </Typography>
+        <Typography
+          variant="p"
+          sx={{
+            p: 1,
+            m: 0,
+            fontWeight: 'bold',
+          }}
+        >
+          Collaborate with others
+        </Typography>
+        <Typography
+          variant="p"
+          sx={{
+            p: 1,
+            m: 0,
+          }}
+        >
+          You can give access to another user to collaborate on a stack and the
+          todo lists in it.
+          <br />
+          Go into the stack and hit the Edit Stack button, and you will find a
+          search field. Type the name or username of the person you want to
+          share the stack with, and choose the person from the list. If you
+          can&apos;t find them there, you may want to ask them to register an
+          account.
+        </Typography>
+        {!isAuthenticated && (
+          <>
+            <Typography
+              variant="p"
+              sx={{
+                p: 1,
+                m: 0,
+              }}
+            >
+              You need to be logged in to use this app. Let&apos;s get started!
+            </Typography>
+            <Button sx={{ alignSelf: 'center' }} component={Link} to="/login">
+              Login or register
+            </Button>
+          </>
+        )}
+      </Box>
+      <Box>
+        <Box
+          sx={{
+            my: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+          }}
+        >
+          <LogoCard size="sm" />
+          <Typography
+            variant="h6"
+            sx={{
+              p: 0,
+              m: 0,
+              fontWeight: 'bold',
+            }}
+          >
+            STICKY NOTE
+          </Typography>
+        </Box>
+      </Box>
+      <Box
+        component="footer"
+        sx={{
+          position: 'fixed',
+          width: '100vw',
+          bottom: 0,
+          left: 0,
+          p: 2,
+          mt: 'auto',
+          borderTop: '1px solid #ffbfd1',
+        }}
+      >
+        <Container
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Typography>Maja Thunberg 2021</Typography>
+          <IconButton href="https://github.com/fermentedcat/todo-app">
+            <GitHubIcon sx={{ color: 'rgb(25, 118, 210)' }} />
+          </IconButton>
+        </Container>
+      </Box>
+    </Container>
   )
 }
