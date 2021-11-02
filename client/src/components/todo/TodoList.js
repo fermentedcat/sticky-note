a6018c705d73564db3bfad718f3e594e9315d4c9
Maja Thunberg <majaneko@gmail.com>
2021-11-02 18:17:46 +0100

    Render 'nothing here' if todos is empty array

--- client/src/components/todo/TodoList.js
+++ client/src/components/todo/TodoList.js
@@ -1,7 +1,42 @@
 import React from 'react'
 import { Grid, Box, Typography, Button } from '@mui/material'
 import TodoItem from './TodoItem'
-// import IconButton from '../button/IconButton'
+
+const gridItemStyle = {
+  height: '100%',
+  boxShadow: '5px 0px 19px 0px rgba(255,191,209,0.34)',
+  paddingTop: 14,
+  overflow: 'scroll',
+  '&::-webkit-scrollbar': {
+    width: '0.25em',
+  },
+  '&::-webkit-scrollbar-thumb': {
+    backgroundColor: '#ffbfd173',
+  },
+}
+
+const boxStyle = {
+  padding: 2,
+  boxSizing: 'border-box',
+  width: '100%',
+  height: '2em',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+}
+
+const titleStyle = {
+  marginLeft: 0.5,
+  fontSize: '14px',
+  fontWeight: 'bold',
+  color: 'rgb(25, 118, 210)',
+}
+
+const descriptionStyle = {
+  marginLeft: 2,
+  fontSize: '14px',
+  color: 'rgb(25, 118, 210)',
+}
 
 export default function TodoList({
   stack,
@@ -11,49 +46,31 @@ export default function TodoList({
   title,
 }) {
   return (
-    <Grid
-      item
-      xs
-      sx={{
-        height: '100%',
-        boxShadow: '5px 0px 19px 0px rgba(255,191,209,0.34)',
-        paddingTop: 14,
-        overflow: 'scroll',
-        '&::-webkit-scrollbar': {
-          width: '0.25em',
-        },
-        '&::-webkit-scrollbar-thumb': {
-          backgroundColor: '#ffbfd173',
-        },
-      }}
-    >
+    <Grid item xs sx={gridItemStyle}>
       <Box sx={{ flexGrow: 1 }}>
-        <Box
-          sx={{
-            padding: 2,
-            boxSizing: 'border-box',
-            width: '100%',
-            height: '2em',
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}
-        >
-          {title && (
-            <Typography
-              variant="h6"
-              component="h2"
-              align="center"
-              sx={{
-                marginLeft: 0.5,
-                fontSize: '14px',
-                fontWeight: 'bold',
-                color: 'rgb(25, 118, 210)',
-              }}
-            >
-              {title}
-            </Typography>
-          )}
+        <Box sx={boxStyle}>
+          <Box sx={{ display: 'flex' }}>
+            {title && (
+              <Typography
+                variant="h6"
+                component="h2"
+                align="center"
+                sx={titleStyle}
+              >
+                {title}
+              </Typography>
+            )}
+            {stack && (
+              <Typography
+                variant="h6"
+                component="h2"
+                align="center"
+                sx={descriptionStyle}
+              >
+                {stack.description}
+              </Typography>
+            )}
+          </Box>
           {stack && (
             <Box>
               <Button onClick={openEditStackHandler}>Edit Stack</Button>
@@ -61,18 +78,32 @@ export default function TodoList({
             </Box>
           )}
         </Box>
-
-        <Grid
-          container
-          spacing={{ xs: 2, md: 3 }}
-          columns={{ xs: 4, sm: 8, md: 12, lg: 16 }}
-          padding={2}
-        >
-          {todos &&
-            todos.map((todo, index) => {
-              return <TodoItem key={index} todo={todo} />
-            })}
-        </Grid>
+        {todos && todos.length === 0 ? (
+          <div style={{ margin: '0 auto', marginTop: '30vh', width: '50vw' }}>
+            <Typography
+              variant="h5"
+              component="h2"
+              align="center"
+              sx={{ color: '#ffbfd1' }}
+            >
+              Nothing here...
+            </Typography>
+          </div>
+        ) : (
+          <>
+            <Grid
+              container
+              spacing={{ xs: 2, md: 3 }}
+              columns={{ xs: 4, sm: 8, md: 12, lg: 16 }}
+              padding={2}
+            >
+              {todos &&
+                todos.map((todo, index) => {
+                  return <TodoItem key={index} todo={todo} />
+                })}
+            </Grid>
+          </>
+        )}
       </Box>
     </Grid>
   )
